b66a876ec56b410432e773b26e1b775add0a4f20
Muhammed demirci <82472683+Muhammeddemircii@users.noreply.github.com>
2025-03-27 17:15:38 +0300

    d

--- src/components/Tasks/AddTask.jsx
+++ src/components/Tasks/AddTask.jsx
@@ -86,37 +86,33 @@ const AddTask = ({ setOpen, setLoading, handleSave, currentUserId, currentUserTe
         setLoading(false);
         return;
       }
-      
-      console.log("Orijinal isim:", name);
-      console.log("Temizlenmiş isim:", cleanName);
-      console.log("İsim uzunluğu:", cleanName.length);
 
-      let selectedDurationId = null;
+       let selectedDurationId = null;
       
-      console.log("SEÇILEN STATÜ:", status);
+      // console.log("SEÇILEN STATÜ:", status);
       
      
-      if (status === "Reddedildi") {
+      // if (status === "Reddedildi") {
       
-        selectedDurationId = "ba861628-2b0d-48cd-6eb0-08dd72b2e88e";
-        console.log(`"Reddedildi" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
-      } else if (status === "Beklemede") {
+      //   selectedDurationId = "ba861628-2b0d-48cd-6eb0-08dd72b2e88e";
+      //   console.log(`"Reddedildi" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
+      // } else if (status === "Beklemede") {
        
-        selectedDurationId = "19841b9d-e98a-474e-6eae-08dd72b2e88e";
-        console.log(`"Beklemede" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
-      } else if (status === "Atandı") {
+      //   selectedDurationId = "19841b9d-e98a-474e-6eae-08dd72b2e88e";
+      //   console.log(`"Beklemede" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
+      // } else if (status === "Atandı") {
       
-        selectedDurationId = "0fc8818d-27a3-4e8b-6eaf-08dd72b2e88e";
-        console.log(`"Atandı" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
-      } else if (status === "Tamamlandı") {
+      //   selectedDurationId = "0fc8818d-27a3-4e8b-6eaf-08dd72b2e88e";
+      //   console.log(`"Atandı" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
+      // } else if (status === "Tamamlandı") {
        
-        selectedDurationId = "9f3fd5a1-7f18-4e27-6eb1-08dd72b2e88e";
-        console.log(`"Tamamlandı" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
-      } else {
+      //   selectedDurationId = "9f3fd5a1-7f18-4e27-6eb1-08dd72b2e88e";
+      //   console.log(`"Tamamlandı" statüsü için sabit kategori ID kullanılıyor: ${selectedDurationId}`);
+      // } else {
        
-        selectedDurationId = "19841b9d-e98a-474e-6eae-08dd72b2e88e"; 
-        console.warn(`Tanınmayan statü: ${status}, varsayılan "Beklemede" kategorisi kullanılıyor`);
-      }
+      //   selectedDurationId = "19841b9d-e98a-474e-6eae-08dd72b2e88e"; 
+      //   console.warn(`Tanınmayan statü: ${status}, varsayılan "Beklemede" kategorisi kullanılıyor`);
+      // }
       
      
       const formData = new FormData();
@@ -260,7 +256,7 @@ const AddTask = ({ setOpen, setLoading, handleSave, currentUserId, currentUserTe
             <MenuItem value="5">Çok Yüksek</MenuItem>
           </Select>
         </FormControl>
-
+{/* 
         <FormControl>
           <InputLabel id="status-label">Statü</InputLabel>
           <Select
@@ -275,7 +271,7 @@ const AddTask = ({ setOpen, setLoading, handleSave, currentUserId, currentUserTe
             <MenuItem value="Tamamlandı">Tamamlandı</MenuItem>
             <MenuItem value="Reddedildi">Reddedildi</MenuItem>
           </Select>
-        </FormControl>
+        </FormControl> */}
 
         <div className="task-buttons">
           <Button onClick={handleCloseAddTask}>Kapat</Button>
